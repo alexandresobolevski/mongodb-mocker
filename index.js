4f2818eeda7afec1666f7eb946f3f6de7be4f69a
Alexandre Sobolevski <sobolevski.a@gmail.com>
2018-11-12 10:04:04 -0500

    Refactor to use async/await pattern

--- index.js
+++ index.js
@@ -8,132 +8,145 @@ const { MongoClient } = mongodb;
 const getPort = promisify(portfinder.getPort);
 const localhost = '127.0.0.1';
 
+const defaultConfig = {
+  host: localhost,
+  dbPath: path.join(__dirname, './.data'),
+  storageEngine: 'ephemeralForTest',
+  autoShutdown: true,
+  started: false,
+  port: 8001,
+  maxRetries: 5,
+  shutDownWait: 100,
+};
+
 class mongoDbMockedServer {
   constructor(opts = {}) {
-    this.config = {
-      host: localhost,
-      dbPath: path.join(__dirname, './.data'),
-      storageEngine: 'ephemeralForTest',
-      autoShutdown: true,
-      started: false,
-      port: null,
-      maxRetries: opts.maxRetries || 5,
-    };
+    this.config = Object.assign(defaultConfig, opts);
     this.mongoClient = opts.mongoClient || MongoClient;
     this.connectionsCache = {};
     this.serverEmitter = null;
     this.clients = [];
   }
 
-  startMongoServer(retries = 0, cb) {
-    Promise.resolve()
-      .then(() => {
-        if (this.config.port) {
-          return Promise.resolve(this.config.port);
+  async startMongoServer(retries = 0, cb) {
+    try {
+      let { port } = this.config;
+      if (!port) {
+        port = await getPort();
+      }
+
+      this.config.port = port;
+      const {
+        storageEngine,
+        host,
+        dbPath,
+        maxRetries,
+        autoShutdown,
+      } = this.config;
+
+      const serverParams = {
+        args: {
+          storageEngine,
+          bind_ip: host,
+          port,
+          dbpath: dbPath,
+        },
+        auto_shutdown: autoShutdown,
+      };
+
+      this.serverEmitter = mongodbPrebuilt.start_server(serverParams, (error) => {
+        if (error === 'EADDRINUSE' && retries < maxRetries) {
+          setTimeout(() => this.startMongoServer(retries + 1, cb), 200);
+          return null;
         }
-        return getPort();
-      })
-      .then((port) => {
-        this.config.port = port;
-        const serverParams = {
-          args: {
-            storageEngine: this.config.storageEngine,
-            bind_ip: this.config.host,
-            port: this.config.port,
-            dbpath: this.config.dbPath,
-          },
-          auto_shutdown: this.config.autoShutdown,
-        };
-        this.serverEmitter = mongodbPrebuilt.start_server(serverParams, (error) => {
-          if (error === 'EADDRINUSE' && retries < this.config.maxRetries) {
-            setTimeout(() => this.startMongoServer(retries + 1, cb), 200);
-            return;
-          }
-          if (!error) {
-            this.config.started = true;
-          }
-          cb(error);
-        });
-      })
-      .catch(e => cb(e));
+        if (error) {
+          return cb(error);
+        }
+        this.config.started = true;
+        return cb();
+      });
+    } catch (e) {
+      cb(e);
+    }
   }
 
-  // Mainly a wrapper of startMongoServer
+
+  // Mainly a wrapper of startMongoServer that recursively tries to
+  // start the server until a max nb of tries is reached
   start() {
     return new Promise((res, rej) => this.startMongoServer(0, (err) => {
-      if (err) { return rej(err); }
+      if (err) {
+        return rej(err);
+      }
       return res(this.config);
     }));
   }
 
-  addConnection(dbName) {
-    return new Promise((resolve, reject) => {
+  async addConnection(dbName) {
+    try {
       if (!this.config.started) {
-        reject(new Error('Can not add connection if server is not started'));
+        throw new Error('can not add connection if server is not started');
       }
       const { host, port } = this.config;
       const uri = `mongodb://${host}:${port}/${dbName}`;
-
-      this.mongoClient.connect(uri, (error, client) => {
-        if (error) { return reject(error); }
-        this.clients.push(client);
-        const conn = client.db(dbName);
-        this.connectionsCache[dbName] = conn;
-        return resolve(conn);
-      });
-    });
+      const client = await this.mongoClient.connect(uri);
+      this.clients.push(client);
+      const conn = client.db(dbName);
+      this.connectionsCache[dbName] = conn;
+      return conn;
+    } catch (e) {
+      throw new Error(`failed to add connection ${e}`);
+    }
   }
 
-  getConnection(dbName) {
-    // Since mongodb 3+, database name is required
-    return new Promise((resolve, reject) => {
+  async getConnection(dbName) {
+    try {
+      // Since mongodb 3+, database name is required
       // If connection to database is cached return it
       if (this.connectionsCache[dbName]) {
-        return resolve(this.connectionsCache[dbName]);
+        return this.connectionsCache[dbName];
       }
 
-      return Promise.resolve()
-        // If we do not have a configuration, start the server
-        .then(() => {
-          if (this.config) {
-            return Promise.resolve();
-          }
-          return this.start();
-        })
-        // Set new config if needed, add new connection and return
-        .then(() => this.addConnection(dbName))
-        .then(conn => resolve(conn))
-        .catch(e => reject(e));
-    });
+      // If we do not have a configuration, start the server
+      if (!this.config.started) {
+        await this.start();
+      }
+
+      // Set new config if needed, add new connection and return
+      const conn = await this.addConnection(dbName);
+      return conn;
+    } catch (e) {
+      throw new Error(`failed to get connection ${e}`);
+    }
   }
 
   // TODO:
-  // Method to delete all collection records in the database dbName connection
-  // If dbName = null, delete all collections in all connections in cache
-  // reset(dbName = null) {
-  // }
+  // reset() {}
 
-  shutDown() {
-    return new Promise((resolve, reject) => {
+  async shutDown() {
+    try {
       if (this.serverEmitter) {
         this.serverEmitter.emit('mongoShutdown');
         this.config.started = false;
       }
 
-      // Let the event propagate and close the server before deleting
-      setTimeout(() => {
-        const closePromises = [];
-        const clients = this.clients.splice(0);
-        clients.forEach((client) => {
-          if (client.close) { closePromises.push(client.close()); }
-        });
-        return Promise.all(closePromises).then(() => {
-          this.serverEmitter = null;
-          this.connectionsCache = {};
-          return resolve();
-        }).catch(e => reject(e));
-      }, 100);
-    });
+      // Let the event propagate and close the server before deleting clients
+      await new Promise(r => setTimeout(r, this.config.shutDownWait));
+      const closePromises = [];
+      const clients = this.clients.splice(0);
+      clients.forEach((client) => {
+        if (client.close) {
+          closePromises.push(client.close());
+        }
+      });
+      await Promise.all(closePromises);
+
+      this.serverEmitter = null;
+      this.connectionsCache = {};
+      return null;
+    } catch (e) {
+      throw new Error(`failed to shutdown database ${e}`);
+    }
   }
 }
 
--- test.js
+++ test.js
@@ -25,56 +25,49 @@ describe('MongodbMocker', () => {
 
   describe('.startMongoServer', () => {
     it('catches an error coming from obtainig a free port', () =>
-      mongodbMocker
-        .startMongoServer(0, (err) => {
-          if (err) { return Promise.reject(err); }
-          should.exist(mongodbMocker.config);
-          mongodbMocker.config.started.should.be.true;
-          return Promise.resolve();
-        }));
+      mongodbMocker.startMongoServer(0, (err) => {
+        if (err) { throw new Error(err); }
+        should.exist(mongodbMocker.config);
+        mongodbMocker.config.started.should.be.true;
+        return Promise.resolve();
+      }));
   });
 
   describe('.start', () => {
-    it('creates an in memory mongodb server and returns its config ' +
-      'object', () => mongodbMocker
-      .start()
-      .then(() => {
-        mongodbMocker.config.started.should.be.true;
-        mongodbMocker.config.should.be.an('object');
-        mongodbMocker.config.should.have.property('port');
-        mongodbMocker.config.should.have.property('host');
-        mongodbMocker.config.host.should.equal('127.0.0.1');
-      }));
-
-    it('retires to connect a default of 5 times if the server is in use, ' +
-      'returns error if unsuccessful after 5 tries', () => {
-      stubbedMethod = sinon.stub(mongodbPrebuilt, 'start_server');
-      stubbedMethod.yields('EADDRINUSE');
-      return mongodbMocker
-        .start()
-        .catch((e) => {
-          should.exist(e);
-          e.should.equal('EADDRINUSE');
-          mongodbMocker.config.started.should.be.false;
-        });
+    it('creates an in memory mongodb server and returns its config object', async () => {
+      await mongodbMocker.start();
+      mongodbMocker.config.started.should.be.true;
+      mongodbMocker.config.should.be.an('object');
+      mongodbMocker.config.should.have.property('port');
+      mongodbMocker.config.should.have.property('host');
+      mongodbMocker.config.host.should.equal('127.0.0.1');
     });
 
-    it('retires to connect a default of 5 times if the server is in use', () => {
+    it('retires to connect a default of 5 times if the server is in use, ' +
+      'returns error if unsuccessful after 5 tries', async () => {
+      stubbedMethod = sinon.stub(mongodbPrebuilt, 'start_server');
+      stubbedMethod.yields('EADDRINUSE');
+      try {
+        await mongodbMocker.start();
+      } catch (e) {
+        e.should.equal('EADDRINUSE');
+        mongodbMocker.config.started.should.be.false;
+      }
+    });
+
+    it('retires to connect a default of 5 times if the server is in use', async () => {
       stubbedMethod = sinon.stub(mongodbPrebuilt, 'start_server');
       stubbedMethod.onCall(0).yields('EADDRINUSE');
       stubbedMethod.onCall(1).yields('EADDRINUSE');
       stubbedMethod.onCall(2).yields('EADDRINUSE');
       stubbedMethod.onCall(3).yields('EADDRINUSE');
       stubbedMethod.onCall(4).yields(null);
-      return mongodbMocker
-        .start()
-        .then(() => {
-          mongodbMocker.config.should.be.an('object');
-          mongodbMocker.config.started.should.be.true;
-          mongodbMocker.config.should.have.property('port');
-          mongodbMocker.config.should.have.property('host');
-          mongodbMocker.config.host.should.equal('127.0.0.1');
-        });
+      await mongodbMocker.start();
+      mongodbMocker.config.should.be.an('object');
+      mongodbMocker.config.started.should.be.true;
+      mongodbMocker.config.should.have.property('port');
+      mongodbMocker.config.should.have.property('host');
+      mongodbMocker.config.host.should.equal('127.0.0.1');
     });
   });
 
@@ -82,75 +75,47 @@ describe('MongodbMocker', () => {
   describe('.getConnection', () => {
     it('starts an in memory mongodb server, and creates a connection ' +
         'to provided database, returns its connection which can be used ' +
-        'to store documents', () =>
-      mongodbMocker
-        .start()
-        .then(() => mongodbMocker.getConnection('some-database'))
-        .then((conn) => {
-          should.exist(conn);
-          should.exist(conn.s.databaseName);
-          conn.s.databaseName.should.equal('some-database');
-          dbConnection = conn;
-          return conn
-            .collection('new-collection')
-            .insertMany([{ foo: 'bar' }, { foo: 'biz' }], (err, inserted) => {
-              if (err) { return Promise.reject(err); }
-              inserted.result.n.should.equal(2);
-              return Promise.resolve();
-            });
-        })
-        .then(() => new Promise((resolve, reject) =>
-          dbConnection
-            .collection('new-collection')
-            .find({}, { fields: { _id: 0 } })
-            .toArray((err, docs) => {
-              if (err) { return reject(err); }
-              return resolve(docs);
-            })))
-        .then((docs) => {
-          should.exist(docs);
-          docs.length.should.equal(2);
-          should.exist(mongodbMocker.connectionsCache['some-database']);
-          docs.should.deep.equal([{ foo: 'bar' }, { foo: 'biz' }]);
-        }));
+        'to store documents', async () => {
+      await mongodbMocker.start();
+      const conn = await mongodbMocker.getConnection('some-database');
+      should.exist(conn);
+      should.exist(conn.s.databaseName);
+      conn.s.databaseName.should.equal('some-database');
+      dbConnection = conn;
+      await conn.collection('new-collection').insertMany([{ foo: 'bar' }, { foo: 'biz' }]);
+      const docs = await dbConnection.collection('new-collection').find({}, { fields: { _id: 0 } }).toArray();
+      should.exist(docs);
+      docs.length.should.equal(2);
+      should.exist(mongodbMocker.connectionsCache['some-database']);
+      docs.should.deep.equal([{ foo: 'bar' }, { foo: 'biz' }]);
+    });
 
     it('does not start an in memory mongodb server if alredy started, ' +
-        'simply creates a connection to provided database', () => {
-      mongodbMocker
-        .start()
-        .then(() => {
-          mongodbMocker.start = sinon.spy();
-          return Promise.resolve();
-        })
-        .then(() => mongodbMocker.getConnection('some-database'))
-        .then((conn) => {
-          should.exist(conn);
-          should.exist(conn.s.databaseName);
-          should.exist(mongodbMocker.connectionsCache['some-database']);
-          conn.s.databaseName.should.equal('some-database');
-          mongodbMocker.start.called.should.be.false;
-        });
+        'simply creates a connection to provided database', async () => {
+      await mongodbMocker.start();
+      stubbedMethod = sinon.stub(mongodbMocker, 'start');
+      const conn = await mongodbMocker.getConnection('some-database');
+      should.exist(conn);
+      should.exist(conn.s.databaseName);
+      should.exist(mongodbMocker.connectionsCache['some-database']);
+      conn.s.databaseName.should.equal('some-database');
+      mongodbMocker.start.called.should.be.false;
     });
 
     it('does not start an in memory mongodb server if alredy started, ' +
         'if connection to provided database was already created, ' +
-        'simply return it from the cache', () =>
-      mongodbMocker
-        .start()
-        .then(() => mongodbMocker.getConnection('some-database'))
-        .then(() => {
-          mongodbMocker.start = sinon.spy();
-          mongodbMocker.addConnection = sinon.spy();
-          return Promise.resolve();
-        })
-        .then(() => mongodbMocker.getConnection('some-database'))
-        .then((conn) => {
-          should.exist(conn);
-          should.exist(conn.s.databaseName);
-          conn.s.databaseName.should.equal('some-database');
-          should.exist(mongodbMocker.connectionsCache['some-database']);
-          mongodbMocker.start.called.should.be.false;
-          mongodbMocker.addConnection.called.should.be.false;
-        }));
+        'simply return it from the cache', async () => {
+      await mongodbMocker.start();
+      let conn = await mongodbMocker.getConnection('some-database');
+      stubbedMethod = sinon.stub(mongodbMocker, 'start');
+      mongodbMocker.addConnection = sinon.spy();
+      conn = await mongodbMocker.getConnection('some-database');
+      should.exist(conn);
+      should.exist(conn.s.databaseName);
+      conn.s.databaseName.should.equal('some-database');
+      should.exist(mongodbMocker.connectionsCache['some-database']);
+      mongodbMocker.start.called.should.be.false;
+      mongodbMocker.addConnection.called.should.be.false;
+    });
   });
 });
